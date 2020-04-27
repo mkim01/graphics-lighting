a4bca6d60b465b3e619db1051a4e25be532b24c5
Your Name <mkim01@stuy.edu>
2020-04-27 10:27:11 -0400

    shading function

--- gmath.py
+++ gmath.py
@@ -22,19 +22,76 @@ SPECULAR_EXP = 4
 
 #lighting functions
 def get_lighting(normal, view, ambient, light, areflect, dreflect, sreflect ):
-    return [0, 0, 0]
+    normalize(normal)
+    normalize(light[LOCATION])
+    normalize(view)
+
+    a = calculate_ambient(ambient, areflect)
+    d = calculate_diffuse(light, dreflect, normal)
+    s = calculate_specular(light, sreflect, view, normal)
+
+    i = [0, 0, 0]
+    i[0] = int(a[0] + d[0] + s[0])
+    i[1] = int(a[1] + d[1] + s[1])
+    i[2] = int(a[2] + d[2] + s[2])
+
+    limit_color(i)
+
+    return i
 
 def calculate_ambient(alight, areflect):
-    pass
+    a = [0, 0, 0]
+    a[0] = alight[0] * areflect[0]
+    a[1] = alight[1] * areflect[1]
+    a[2] = alight[2] * areflect[2]
+    return a
 
 def calculate_diffuse(light, dreflect, normal):
-    pass
+    d = [0, 0, 0]
 
+    dProduct = dot_product(normal, light[0])
+
+    if dProduct < 0:
+        dProduct = 0
+
+    d[0] = light[1][0] * dreflect[0] * dProduct
+    d[1] = light[1][1] * dreflect[1] * dProduct
+    d[2] = light[1][2] * dreflect[2] * dProduct
+    return d
 def calculate_specular(light, sreflect, view, normal):
-    pass
+    s = [0, 0, 0]
+    n = [0, 0, 0]
 
+    r = 2 * dot_product(normal, light[0])
+    n[0] = (normal[0] * r) - light[0][0]
+    n[1] = (normal[1] * r) - light[0][1]
+    n[2] = (normal[2] * r) - light[0][2]
+
+    r = dot_product(n, view)
+    if r < 0:
+        r = 0
+    r = pow(r, SPECULAR_EXP)
+
+    s[0] = light[1][0] * sreflect[0] * r
+    s[1] = light[1][1] * sreflect[1] * r
+    s[2] = light[1][2] * sreflect[2] * r
+
+    return s
 def limit_color(color):
-    pass
+    if color[RED] > 255:
+        color[RED] = 255
+    if color[RED] < 0:
+        color[RED] = 0
+
+    if color[GREEN] > 255:
+        color[GREEN] = 255
+    if color[GREEN] < 0:
+        color[GREEN] = 0
+
+    if color[BLUE] > 255:
+        color[BLUE] = 255
+    if color[BLUE] < 0:
+        color[BLUE] = 0
 
 #vector functions
 #normalize vetor, should modify the parameter
